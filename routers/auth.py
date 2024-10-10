ee812c9d3a7729deb3b6efabd909ed1018b6e213
Miguel Angel López Monroy <miguel.lopezm.dev@gmail.com>
2024-10-09 20:02:32 -0600

    Added simple agent authentication

--- routers/auth.py
+++ routers/auth.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
 from fastapi import APIRouter, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from starlette import status
@@ -25,6 +26,15 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+def authenticate_agent(username: str, password: str, db):
+    agent = db.query(Agents).filter(Agents.username == username).first()
+    if not agent:
+        return False
+    if not bcrypt_context.verify(password, agent.hashed_password):
+        return False
+    return True
+
+
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_agent(db: db_dependency, create_agent_request: CreateAgentRequest):
     create_agent_model = Agents(
@@ -38,3 +48,13 @@ async def create_agent(db: db_dependency, create_agent_request: CreateAgentReque
 
     db.add(create_agent_model)
     db.commit()
+
+
+@router.post("/token")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency
+):
+    agent = authenticate_agent(form_data.username, form_data.password, db)
+    if not agent:
+        return "Failed authentication"
+    return "Successful authentication"
